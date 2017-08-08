44820eaf3078d51bb5da95b2dd20bd28707f2569
Adrian Czigany <c.adrian89@gmail.com>
2017-08-08 20:20:03 +0200

    two way binding with ObservableField

--- app/src/main/java/com/adrian/project/ui/jsonplaceholder/submodules/albumspage/viewmodel/AlbumItemViewModel.kt
+++ app/src/main/java/com/adrian/project/ui/jsonplaceholder/submodules/albumspage/viewmodel/AlbumItemViewModel.kt
@@ -1,7 +1,6 @@
 package com.adrian.project.ui.jsonplaceholder.submodules.albumspage.viewmodel
 
-import android.databinding.Bindable
-import com.adrian.project.BR
+import android.databinding.ObservableField
 import com.adrian.project.base.ListItemViewModel
 
 /**
@@ -10,35 +9,12 @@ import com.adrian.project.base.ListItemViewModel
 
 class AlbumItemViewModel(var album: Album) : ListItemViewModel() {
 
-//    var id = album.id
-//    var userId = album.userId
-//    var title = album.title
-
-
-    var id: Int
-        @Bindable get() = album.id
-        set(value) {
-            id = value
-            notifyPropertyChanged(BR.id)
-        }
-
-    var userId: Int
-        @Bindable get() = album.userId
-        set(value) {
-            userId= value
-            notifyPropertyChanged(BR.userId)
-        }
-
-    var title: String
-        @Bindable get() = album.title
-        set(value) {
-            title = value
-            notifyPropertyChanged(BR.title)
-        }
+    var id = album.id
+    var userId = album.userId
+    var title : ObservableField<String> = ObservableField(album.title)
 
     override fun toString(): String {
         return "AlbumItemViewModel(album=$album)"
     }
-
 
 }
